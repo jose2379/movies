202064c2364bc5ce72b53eb4a7036a0dd3b7eed6
Jose M. Minambres Redondo <ext.josemaria.minambresredondo@sgs.com>
2021-11-11 00:26:04 +0100

    Creación de los archivos de configuración de las traducciones y optimización del menú para la carga desde un servicio / api

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { MenuOption } from '../interfaces/interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getMenuOptions(): Observable<MenuOption[]>{
+    return this.httpClient.get<MenuOption[]>('/assets/data/menu-options.json');
+  }
+}
