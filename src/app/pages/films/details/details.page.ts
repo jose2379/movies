0a9b62bdf045dd0dc7f588716b1234de26da9241
Jose M. Minambres Redondo <ext.josemaria.minambresredondo@sgs.com>
2021-11-12 00:23:02 +0100

    Creado el servicio para recoger el parámetro de id de película y mostrar el dato en el header de fimls details page

--- src/app/pages/films/details/details.page.ts
+++ src/app/pages/films/details/details.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Movie } from 'src/app/interfaces/api.interface';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsPage implements OnInit {
 
-  constructor() { }
+  film: Movie = null;
+
+  constructor(private activeRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.film = this.activeRoute.snapshot.data.film;
+    console.log(this.activeRoute.snapshot, this.film);
   }
 
 }
